aec7311632a2f6ed8430fd0541441026f1da3477
ipsitasahu6372@gmail.com <ipsitasahu6372@gmail.com>
2026-02-01 17:09:38 +0530

    delete dev index.html

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Food-Delivery-Website/'
+  base: '/Food-Delivery-Website/',   // 👈 repo name EXACT same
 })
